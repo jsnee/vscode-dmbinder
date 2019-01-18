7444c29586f61961e4e31907a9d5ff3500e10139
Joseph Snee <snoey_92@hotmail.com>
2019-01-18 13:43:19 -0600

    Write markdown changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import { commands, ExtensionContext, QuickPickOptions, Uri, window } from 'vscode';
-import { promptCreateCampaign } from './common';
+import { promptCreateCampaign, homebrewAddWrapper, homebrewReplacePages } from './common';
+//import * as homebrew from './HomebrewRenderer';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -26,6 +27,14 @@ export async function activate(context: ExtensionContext) {
         }
     });
     context.subscriptions.push(addCampaignDisposable);
+    return {
+        extendMarkdownIt(md: any) {
+            //return md.use(require('./HomebrewRenderer'));
+            md.core.ruler.before('replacements', 'homebrewery_wrapper', homebrewAddWrapper);
+            md.core.ruler.after('homebrewery_wrapper', 'homebrewery_pages', homebrewReplacePages);
+            return md;
+        }
+    };
 }
 
 // this method is called when your extension is deactivated
